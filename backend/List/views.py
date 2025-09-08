56135572d6e375bc3c30cee1f4b8006a13c24229
Alamdar1 <alamdarali.900@gmail.com>
2025-09-08 13:39:38 -0400

    Set the crud operation for List

--- backend/List/views.py
+++ backend/List/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import List
+from .serializer import ListSerializer  
 
-# Create your views here.
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    
